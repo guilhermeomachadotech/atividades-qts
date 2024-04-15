e21fd2a9707445b919d4d339de5bc52e6b027e53
Guilherme de Oliveira Machado <112173925+guilhermeomachadotech@users.noreply.github.com>
2024-04-15 20:26:41 -0300

    Atividades da Aula 1

---
+++ Aula1/lista.py
@@ -0,0 +1,20 @@
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista2 = [1, 2, 'junior', 'aline', 4, 4.5]
+print(lista1)
+print(30*'*')
+print(lista2)
+lista2.append('allan')
+print(lista2)
+
+nomes=['Camila', 'luan', 'Debora', 'Edilson']
+print(nomes)
+nomes.insert(2,'Michael')
+print(nomes)
+
+linguagens = ['Python', 'java', 'JavaScript', 'c']
+print(linguagens)
+linguagens[3] = ['c++']
+print (linguagens)
+
+lista2.remove('junior')
+print(lista2)
